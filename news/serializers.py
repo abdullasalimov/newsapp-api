ba11022543c257d8b30759983d02f791b6176f67
unknown <abdullasalimov7@gmail.com>
2022-04-04 17:29:13 +0500

    sadf

--- news/serializers.py
+++ news/serializers.py
@@ -45,6 +45,23 @@ class NewsSerializer(serializers.ModelSerializer):
 
         read_only_fields = ["likes"]
 
+    def to_representation(self, instance):
+        return {
+            "status": 0,
+            "message": "Success",
+            "data": {
+                "updatedAt": "2020-08-31 17:49:15",
+                "serverTime": "2022-03-23 15:10:11",
+                "news": [
+                    {
+                        "id": instance.id,
+                        "title": instance.title,
+                        "created_at": instance.created_at,
+                    }
+                ],
+            },
+        }
+
     def create(self, validated_data):
         category_id = self.context["category_id"]
         return News.objects.create(category_id=category_id, **validated_data)
